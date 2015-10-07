4fb217529e81e2cab954461f611d9e64c9b20784
zldevil2011 <929034478@qq.com>
2015-10-07 17:36:50 +0800

    fix 500 (use the devil to start the server)

--- wsn/wsn/settings.py
+++ wsn/wsn/settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_PATH = './media'
-
+STATIC_ROOT='./static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
