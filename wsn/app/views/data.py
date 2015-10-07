a34f1256599e20e10a40e94a150e9aa485e84c9b
zldevil2011 <929034478@qq.com>
2015-10-07 14:06:55 +0800

    show clouddir use Pie chart

--- wsn/app/views/data.py
+++ wsn/app/views/data.py
@@ -33,7 +33,47 @@ air_parameter_data = {
     }
 }
 cloud_dir = ["E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"]
-
+cloud_dir_data = {
+    "E": {
+        "count": 0
+    },
+    "ESE": {
+        "count": 0
+    },
+    "SE": {
+        "count": 0
+    },
+    "SSE": {
+        "count": 0
+    },
+    "S": {
+        "count": 0
+    },
+    "SSW": {
+        "count": 0
+    },
+    "SW": {
+        "count": 0
+    },
+    "WSW": {
+        "count": 0
+    },
+    "W": {
+        "count": 0
+    },
+    "WNW": {
+        "count": 0
+    },
+    "NW": {
+        "count": 0
+    },
+    "NNW": {
+        "count": 0
+    },
+    "N": {
+        "count": 0
+    }
+}
 def air(request):
     air_type = "pm25"
     try:
@@ -45,7 +85,15 @@ def air(request):
     data = []
     air_list = Air.objects.all()[0:24]
     air_type = str(air_type)
+    main_count = 0
+    for dir in cloud_dir:
+        print dir
+        print cloud_dir_data[dir]["count"]
+        cloud_dir_data[dir]["count"] = 0
+    # return HttpResponse(cloud_dir_data)
+
     for air in air_list:
+        main_count += 1
         if air_type == "pm25":
             data.append(air.pm25)
         elif air_type == "cloud":
@@ -57,14 +105,30 @@ def air(request):
         elif air_type == "guang":
             data.append(air.guang)
         elif air_type == "clouddir":
-            data.append(air.clouddir)
+            data.append(air.pm25)
+            cloud_dir_data[air.clouddir]["count"] += 1
+
+    print main_count
+    cloud_dir_count_data = []
+    for dir in cloud_dir:
+        print cloud_dir_data[dir]["count"]
+        count = {}
+        count["name"] = dir
+        count["y"] = cloud_dir_data[dir]["count"] / (main_count * 1.0)
+        cloud_dir_count_data.append(count)
+        # cloud_dir_count_data[dir] = cloud_dir_data[dir]["count"] / (main_count * 1.0)
+        # print cloud_dir_count_data[dir]
+
+
     title = air_parameter_data[air_type]["name"]
     unit = air_parameter_data[air_type]["unit"]
+    # return HttpResponse(cloud_dir_count_data)
     return render(request, 'air.html', {
         "data": data,
         "title": title,
         "unit": unit,
         "air_type": air_type,
+        "cloud_dir_count_data": cloud_dir_count_data
         })
 
 # Create your views here.
